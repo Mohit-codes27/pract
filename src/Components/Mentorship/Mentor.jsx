8d65c824a4106196c49b37749e294a58a7ae2588
Aaditya <147937815+Aaditya2302@users.noreply.github.com>
2025-03-02 00:04:13 +0530

    Update Mentor.jsx
    correct camel case

--- src/Components/Mentorship/Mentor.jsx
+++ src/Components/Mentorship/Mentor.jsx
@@ -166,7 +166,7 @@ export default function MentorPage() {
         transition={{ duration: 0.8 }}
       >
         <h1 className="text-3xl md:text-4xl font-bold text-gray-800">
-          Get Personalized<span className="text-[#0a66c2]">1:1</span>Mentorship From Industry Experts!
+          Get Personalized <span className="text-[#0a66c2]">1:1</span> Mentorship From Industry Experts!
         </h1>
         <p className="text-gray-600 text-base md:text-xl mt-4">
           Connect with experienced mentors who will guide you through your career.
