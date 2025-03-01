82358257d9583d90dcd802a747599353f4d4b52a
Aaditya <147937815+Aaditya2302@users.noreply.github.com>
2025-03-02 00:03:05 +0530

    Update Book.jsx
    correct camel case

--- src/Components/Session/Book.jsx
+++ src/Components/Session/Book.jsx
@@ -166,7 +166,7 @@ export default function BookPage() {
         transition={{ duration: 0.8 }}
       >
         <h1 className="text-3xl md:text-4xl font-bold text-gray-800">
-          bookA<span className="text-[#0a66c2]">1:1</span>CounsellingSession
+          Book A <span className="text-[#0a66c2]">1:1</span> Counselling Session
         </h1>
         <p className="text-gray-600 text-base md:text-xl mt-4">
           Book a session with experienced mentors across domains & work
